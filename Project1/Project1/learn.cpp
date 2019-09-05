f130062fc3d200a3428d1895a2669862c0c29f78
chopinic <yangguangbus@163.com>
2019-09-05 12:23:42 +0800

    test

--- Project1/Project1/learn.cpp
+++ Project1/Project1/learn.cpp
@@ -80,24 +80,29 @@ int main() {
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 
-	unsigned int VBO,VAO;
+	unsigned int VBO,VAO,VAO2;
 	glGenVertexArrays(1, &VAO);
 	glBindVertexArray(VAO);
+	//glBindVertexArray(VAO2);
+	//glGenVertexArrays(1, &VAO2);
 
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Myvertices), Myvertices, GL_STATIC_DRAW);
 
 	//¶¥µã¸ñÊ½£º
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
 	glEnableVertexAttribArray(0);
-
+	int cot = 0;
 	while (!glfwWindowShouldClose(window)) {
 		processInput(window);
-
+		cot++;
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
-
+		if (cot > 100)
+		{
+			glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+		}
 		glUseProgram(shaderProgram);
 
 		/*
